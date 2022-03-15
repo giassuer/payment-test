71eb78c6f0d039cd00033a1fb434a775b42ae530
giassuer <g.assuero@hotmail.it>
2022-03-15 16:45:32 +0100

    first commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <WorkInProgress />
+    </div>
+</template>
+
+<script>
+import WorkInProgress from '../components/WorkInProgress.vue';
+export default {
+    name: 'App',
+    components: {
+        WorkInProgress
+    }
+}
+</script>
